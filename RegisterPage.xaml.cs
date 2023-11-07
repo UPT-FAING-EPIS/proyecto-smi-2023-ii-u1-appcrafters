29e46e1c84b1df86430642e7f47f50b3a2a8d84c
FarleyViveros <farviv123@gmail.com>
2023-11-07 12:49:28 -0500

    VERSION 2

---
+++ RegisterPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using EmailAuth.ViewModels;
+
+namespace EmailAuth
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+            BindingContext = new RegisterViewModel(Navigation);
+            ToolbarItems.Add(new ToolbarItem
+            {
+                IconImageSource = "/Resources/Images/retroceder.jpg", // Aquí debes especificar la ruta de la imagen para el botón de retroceso
+                Text = "Retroceder",
+                Order = ToolbarItemOrder.Primary,
+                Priority = 0
+            });
+
+            // Manejar el evento de hacer clic en el botón de retroceso
+            ToolbarItems[0].Clicked += (sender, e) =>
+            {
+                Navigation.PopAsync(); // Esto retrocederá a la página anterior
+            };
+        }
+    }
+}
